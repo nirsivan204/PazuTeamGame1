e63957c07b9c33999c155d3723d6ce5bfd30c5be
nir sivan <nirsivan204@gmail.com>
2024-01-25 23:40:21 +0200

    controller

--- Assets/Scripts/HumanPlayer.cs
+++ Assets/Scripts/HumanPlayer.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class HumanPlayer : MonoBehaviour
 {
     [SerializeField] int playerIndex;
-    float movementXRight;
-    float movementYRight;
-    float movementXLeft;
-    float movementyLeft;
+
+    public UnityEvent OnXPress;
+    public UnityEvent OnCirclePress;
+    public UnityEvent OnSquarePress;
+    public UnityEvent OnTrianglePress;
+    public UnityEvent<Vector2>  OnRightAnalogMove;
+    public UnityEvent<Vector2> OnLeftAnalogMove;
+
+
+    public float movementXRight;
+    public float movementYRight;
+    public float movementXLeft;
+    public float movementyLeft;
+    
 
     public void init(InputMGR inputMGR)
     {
@@ -19,26 +30,32 @@ public class HumanPlayer : MonoBehaviour
     {
         movementXLeft = movementVector.x;
         movementyLeft = movementVector.y;
+        OnLeftAnalogMove.Invoke(movementVector);
     }
 
     private void OnMoveRight(Vector2 movementVector)
     {
         movementXRight = movementVector.x;
         movementYRight = movementVector.y;
+        OnRightAnalogMove.Invoke(movementVector);
     }
     public void OnFire()
     {
+        OnXPress.Invoke();
     }
 
     public void OnCircle()
     {
-
+        OnCirclePress.Invoke();
     }
     public void OnTriangle()
     {
+        OnTrianglePress.Invoke();
 
     }
     public void OnSquare()
     {
+        OnSquarePress.Invoke();
+
     }
 }
