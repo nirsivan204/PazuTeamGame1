e7fa560d774c500a84cda5efb0c698a069496354
DekelPazuGames <dekel@pazugames.com>
2024-01-26 10:38:12 +0200

    Dekel: Leantween

---
+++ Assets/Scripts/Legs/PlatformMoving.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Legs
+{
+    public class PlatformMoving : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+
+
+    }
+}
