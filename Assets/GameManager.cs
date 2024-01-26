aded0e2eee9cc4222fd015a22cdf35a62ba6634d
nirsivan204 <nirsivan204@gmail.com>
2024-01-26 21:51:57 +0200

    scene fix

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour
     bool isInit;
     public void Awake()
     {
-        InputMGR[] allInputManagers = FindObjectsOfType<InputMGR>();
+        GameManager[] allInputManagers = FindObjectsOfType<GameManager>();
         if (allInputManagers.Length > 1)
         {
             if (!isInit)
@@ -19,14 +19,27 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-            isInit = true;
+            if (!isInit)
+            {
+                isInit = true;
+                DontDestroyOnLoad(this);
+                if (SceneManager.GetActiveScene().buildIndex == 1)
+                {
+                    SceneManager.LoadScene(0, LoadSceneMode.Additive);
+
+                }
+                else
+                {
+                    SceneManager.LoadScene(1, LoadSceneMode.Additive);
+
+                }
+                print(SceneManager.GetActiveScene().buildIndex);
+            }
+
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex == 0 ? 1 : 0, LoadSceneMode.Additive);
         }
     }
 
-    void Start()
-    {
-        SceneManager.LoadScene(1 - SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Additive);
-    }
 
     // Update is called once per frame
     void Update()
