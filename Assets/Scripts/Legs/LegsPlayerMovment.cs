00f44f14ade488e51aefb93df8fbe20b8351c430
Dfarchi <yuvalfarchi7@gmail.com>
2024-01-26 10:39:23 +0200

    yuval: scene

--- Assets/Scripts/Legs/LegsPlayerMovment.cs
+++ Assets/Scripts/Legs/LegsPlayerMovment.cs
@@ -1,5 +1,3 @@
-using Legs;
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,7 +7,6 @@ public class LegsPlayerMovment : AbstractPlayerMovement
     [SerializeField] private float _movmentSpeed = 8f;
     [SerializeField] private float _jumpSpeed = 15f;
     [SerializeField] private RumbleManager rumbleManager;
-    public static LegsPlayerMovment Instance { get; private set; }
 
     private HumanPlayer _controller1;
     private HumanPlayer _controller2;
@@ -17,30 +14,10 @@ public class LegsPlayerMovment : AbstractPlayerMovement
     private Vector2 _movement;
     private Rigidbody2D _rb;
 
-    private bool _isBeingCheerd = true;
-    private bool _isCheering = false;
-
-    private float _jumpCount = 0;
-
-    private float _cheerTime = 2f;
-    public event Action OnCheerAction;
-    public event Action OnCheerEndAction;
-
     private void Awake()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-        }
         _rb = GetComponent<Rigidbody2D>();
-
-        if(TopPlayerController.Instance != null)
-        {
-            TopPlayerController.Instance.OnCheerAction += () => _isBeingCheerd = true;
-            TopPlayerController.Instance.OnCheerEndAction += () => _isBeingCheerd = false;
-        }
     }
-
 
     public override void Init(HumanPlayer controller1, HumanPlayer controller2)
     {
@@ -48,70 +25,26 @@ public class LegsPlayerMovment : AbstractPlayerMovement
         _controller1 = controller1;
         _controller2 = controller2;
         _controller1.OnLeftAnalogMove.AddListener(MoveLeftStick);
-        _controller2.OnXPress.AddListener(CheckJump);
-        //_controller2.OnRightAnalogMove.AddListener(MoveRightStick);
-        _controller2.OnTrianglePress.AddListener(Cheer);
+        _controller1.OnXPress.AddListener(Jump);
+        _controller2.OnRightAnalogMove.AddListener(MoveRightStick);
     }
 
     public void MoveLeftStick(Vector2 movement)
     {
         //print("legsLeft");
-        _movement.x = movement.x * _movmentSpeed;
-        _movement.y = movement.y * _movmentSpeed;
-
+        _movement.y = movement.x * _movmentSpeed;
         _rb.velocity = _movement;
     }
     public void MoveRightStick(Vector2 movement)
     {
         //print("legsRight");
-    }
-
-    private void CheckJump()
-    {
-        if (_jumpCount == 0 || (_jumpCount == 1 & _isBeingCheerd))
-        {
-            Jump();            
-        }
+        _movement.x = movement.y * _movmentSpeed;
+        _rb.velocity = _movement;
     }
 
     private void Jump()
     {
         _movement.y = _jumpSpeed;
         _rb.velocity = _movement;
-        _jumpCount++;
-    }
-
-
-    private void Cheer()
-    {
-        if (_jumpCount != 0)
-            return;
-
-        Debug.Log("Cheer!");
-
-        _isCheering = true;
-        OnCheerAction?.Invoke();
-        this.SetTimer(_cheerTime, () =>
-        {
-            _isCheering = false;
-            OnCheerEndAction?.Invoke();
-        });
-    }    
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "platform")
-        {
-            Debug.Log("platform!");
-            _jumpCount = 0;
-            Platform platform = collision.gameObject.GetComponent<Platform>();
-            transform.parent = platform.gameObject.transform;
-        }
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "platform")
-        {
-            transform.parent = null;
-        }
     }
 }
