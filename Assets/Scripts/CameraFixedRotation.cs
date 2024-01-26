8388c0e44820d1e5250cf02a18e7a8376bf32e06
barakPazu <107786721+BarakPazuGames@users.noreply.github.com>
2024-01-26 20:59:16 +0200

    Barak: Added camera clamping

--- Assets/Scripts/CameraFixedRotation.cs
+++ Assets/Scripts/CameraFixedRotation.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class CameraFixedRotation : MonoBehaviour
 {
+    public float XMin;
+    public float XMax;
+    public float YMin;
+    public float YMax;
+
     void Update()
     {
+        transform.localPosition = Vector3.zero;
         transform.rotation = Quaternion.identity;
+
+        Debug.Log(transform.position);
+        float targetX = Mathf.Clamp(transform.position.x, XMin, XMax);
+        float targetY = Mathf.Clamp(transform.position.y, YMin, YMax);
+        transform.position = new Vector3(targetX, targetY, -10);
+
+        Debug.Log(targetX);
     }
 }
