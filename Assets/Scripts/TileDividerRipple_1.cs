8c0acbf6969d90693abfc5f1f48d80ce38020756
barakPazu <107786721+BarakPazuGames@users.noreply.github.com>
2024-01-27 00:45:34 +0200

    Barak: Added coop split effect

---
+++ Assets/Scripts/TileDividerRipple_1.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileDividerRipple_1 : MonoBehaviour
+{
+    void Start()
+    {
+        LeanTween.scaleY(gameObject, 1.05f, 11).setLoopPingPong();
+    }
+}
