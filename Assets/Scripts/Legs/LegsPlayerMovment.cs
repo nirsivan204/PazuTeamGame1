f5329c4921241b09503aca3a9c3e4c7d102f3fa4
Dfarchi <yuvalfarchi7@gmail.com>
2024-01-26 11:34:34 +0200

    yuval: rega

--- Assets/Scripts/Legs/LegsPlayerMovment.cs
+++ Assets/Scripts/Legs/LegsPlayerMovment.cs
@@ -1,3 +1,4 @@
+using Legs;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,10 +15,22 @@ public class LegsPlayerMovment : AbstractPlayerMovement
     private Vector2 _movement;
     private Rigidbody2D _rb;
 
+    private bool _isBeingCheerd = true;
+    private bool _isGrounded = false;
+    private bool _isDoubleJump = false;
+    private bool _isCheering = false; 
+
+    public static LegsPlayerMovment Instance { get; private set; }
+
     private void Awake()
     {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
         _rb = GetComponent<Rigidbody2D>();
     }
+
 
     public override void Init(HumanPlayer controller1, HumanPlayer controller2)
     {
@@ -25,8 +38,10 @@ public class LegsPlayerMovment : AbstractPlayerMovement
         _controller1 = controller1;
         _controller2 = controller2;
         _controller1.OnLeftAnalogMove.AddListener(MoveLeftStick);
-        _controller1.OnXPress.AddListener(Jump);
-        _controller2.OnRightAnalogMove.AddListener(MoveRightStick);
+        _controller2.OnXPress.AddListener(Jump);
+        //_controller2.OnRightAnalogMove.AddListener(MoveRightStick);
+        _controller2.OnTrianglePress.AddListener(Cheer);
+        _controller1.OnTrianglePress.AddListener(Cheer); // OtherPlayer
     }
 
     public void MoveLeftStick(Vector2 movement)
@@ -46,5 +61,27 @@ public class LegsPlayerMovment : AbstractPlayerMovement
     {
         _movement.y = _jumpSpeed;
         _rb.velocity = _movement;
+    }
+    private void Cheer()
+    {
+        _isCheering = true;
+        LeanTween.delayedCall(gameObject, 2f, () => _isCheering = false);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "platform")
+        {
+            _isGrounded = true;
+            Platform platform = collision.gameObject.GetComponent<Platform>();
+            transform.parent = platform.gameObject.transform;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "platform")
+        {
+            _isGrounded = false;
+            transform.parent = null;
+        }
     }
 }
