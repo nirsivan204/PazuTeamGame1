044e67d9531f5bf99f46884364e040c8c8228dc4
barakPazu <107786721+BarakPazuGames@users.noreply.github.com>
2024-01-26 16:50:11 +0200

    Barak: Added gap jumping

--- Assets/Scripts/StunOrb.cs
+++ Assets/Scripts/StunOrb.cs
@@ -6,8 +6,6 @@ public class StunOrb : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("OnCollisionEnter2D: " + collision.gameObject.name);
-        Debug.Log(collision is IStunnable);
         if(collision.gameObject.GetComponent<IStunnable>() is IStunnable stunnable)
         {
             stunnable.OnStun(StunAmount);
