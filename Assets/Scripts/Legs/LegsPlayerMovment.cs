f1ea8e44058363670ed0834aa5c2d15208ac4fda
Dfarchi <yuvalfarchi7@gmail.com>
2024-01-26 10:39:23 +0200

    yuval: scene

--- Assets/Scripts/Legs/LegsPlayerMovment.cs
+++ Assets/Scripts/Legs/LegsPlayerMovment.cs
@@ -32,13 +32,13 @@ public class LegsPlayerMovment : AbstractPlayerMovement
     public void MoveLeftStick(Vector2 movement)
     {
         //print("legsLeft");
-        _movement.y = movement.y * _movmentSpeed;
+        _movement.y = movement.x * _movmentSpeed;
         _rb.velocity = _movement;
     }
     public void MoveRightStick(Vector2 movement)
     {
         //print("legsRight");
-        _movement.x = movement.x * _movmentSpeed;
+        _movement.x = movement.y * _movmentSpeed;
         _rb.velocity = _movement;
     }
 
