9367ec30e0c5206df7a0f38595d4d40aaa4b3133
OhMyCodeIsTaken <ronbandelmusic@gmail.com>
2024-01-27 12:21:02 +0200

    New Scrolling BGs
    * Now is less dynamic but more efficient and works even in Scene View.
    * Sizes of individual BGs will need to be adjusted for camera zoom level.

--- Assets/Scripts/Background/BackgroundScroller.cs
+++ Assets/Scripts/Background/BackgroundScroller.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class BackgroundScroller : MonoBehaviour
 {
     [SerializeField] private Transform _otherBackground;
-    [SerializeField] float _yLoopResetOffset;
+    [SerializeField] float _ySize;
     [SerializeField] Vector3 _movementVector;
     [SerializeField] Renderer _renderer;
 
+    public float YSize { get => _ySize; }
+
     private void Start()
     {
-        _yLoopResetOffset = _renderer.bounds.size.y;
+        _ySize = _renderer.bounds.size.y;
     }
 
     // Update is called once per frame
@@ -20,15 +22,5 @@ public class BackgroundScroller : MonoBehaviour
         transform.position = transform.position + _movementVector * Time.deltaTime;
     }
 
-    private void OnBecameInvisible()
-    {
-        if(Application.isPlaying)
-        {
-            Debug.Log("please");
-            if (_otherBackground != null)
-            {
-                transform.position = _otherBackground.transform.position - new Vector3(0, _yLoopResetOffset, 0);
-            }           
-        }        
-    }
+    
 }
