72d212594ae63cfb182d31747d69248c357eafb3
Kostya <sydoruk.kostya@gmail.com>
2024-01-27 15:28:57 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Assets/Scripts/CameraOffseter.cs
+++ Assets/Scripts/CameraOffseter.cs
@@ -21,6 +21,7 @@ public class CameraOffseter : MonoBehaviour
         var currentPosition = transform1.position;
         //float targetX = Mathf.Clamp(transform.position.x, XMin, XMax);
         //float targetY = Mathf.Clamp(player.transform.position.y, Mathf.Max(YMin, chaser.transform.position.y + 7), YMax);
-        transform1.position = new Vector3(currentPosition.x, player.transform.position.y + yOffset, -10);
+        transform1.position = new Vector3(currentPosition.x,
+            Mathf.Max(player.transform.position.y, chaser.transform.position.y + yOffset+2) + yOffset, -10);
     }
 }
